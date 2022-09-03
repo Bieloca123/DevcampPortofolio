5f3adfdfa905c85590fd5f6978eb1d5216f24f8e
Bieloca123 <gabrielfreitasdfc123@icloud.com>
2022-09-03 18:43:01 -0300

    Clean controller routes

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,40 +1,32 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[ show edit update destroy ]
 
-  # GET /blogs or /blogs.json
   def index
     @blogs = Blog.all
   end
 
-  # GET /blogs/1 or /blogs/1.json
   def show
   end
 
-  # GET /blogs/new
   def new
     @blog = Blog.new
   end
 
-  # GET /blogs/1/edit
   def edit
   end
 
-  # POST /blogs or /blogs.json
   def create
     @blog = Blog.new(blog_params)
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to blog_url(@blog), notice: "Blog was successfully created." }
-        format.json { render :show, status: :created, location: @blog }
+        format.html { redirect_to @blog, notice: "Blog was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /blogs/1 or /blogs/1.json
   def update
     respond_to do |format|
       if @blog.update(blog_params)
@@ -47,7 +39,6 @@ class BlogsController < ApplicationController
     end
   end
 
-  # DELETE /blogs/1 or /blogs/1.json
   def destroy
     @blog.destroy
 
@@ -58,12 +49,10 @@ class BlogsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_blog
       @blog = Blog.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def blog_params
       params.require(:blog).permit(:title, :body)
     end
