9e53996dc4f28bb0a8c685875a5f10e15ae48224
Bieloca123 <gabrielfreitasdfc123@icloud.com>
2022-09-11 22:28:02 -0300

    custom routes for portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  
+  root to: 'pages#home'
 
   get 'about' , to: 'pages#about'
   get 'pages' , to: 'pages#contact'
 
-  resources :portofolios
+  resources :portofolios, except: [:show]
+  get 'portofolio/:id', to: 'portofolios#show', as: 'portofolio_show'
 
   resources :blogs
 
-  root to: 'pages#home'
 end
